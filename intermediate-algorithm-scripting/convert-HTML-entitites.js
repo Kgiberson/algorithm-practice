0384c8528dd693caf0d8e9606be97f0e8ff63779
Ryan Ramirez <ryan2013@stanford.edu>
2016-08-24 15:20:56 -0700

    Undo push to master

--- intermediate-algorithm-scripting/convert-HTML-entitites.js
+++ intermediate-algorithm-scripting/convert-HTML-entitites.js
@@ -1,19 +1,3 @@
 // Convert the characters &, <, >, ", and '', in a string to their corresponding HTML entities
 
 
-function convertHTML(str) {
-  htmlEntities = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    '\'': '&apos;'
-  };
-  
-  
-  return str.split('').map(function(entity) {
-    return htmlEntities[entity] || entity;
-  }).join('');
-}
-
-convertHTML("Dolce & Gabbana");
