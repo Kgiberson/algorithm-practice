aa81ccf5f9f837c4eac42e3aa3bb699a6f16ce8c
Kari Giberson <kari.giberson@gmail.com>
2016-10-03 22:03:00 -0700

    Add partial smallest common multiple algo

---
+++ intermediate-algorithm-scripting/smallest-common-multiple.js
@@ -0,0 +1,3 @@
+// Find the smallest common muliple of the provided 
+// parameters that can be evenly divided by both, as well 
+// as by all sequential numbers in the range between these parameters.
