85a97b86000c80e62689ff7ddc45ab9a6fa1d94a
Kari Giberson <kari.giberson@gmail.com>
2016-11-09 20:39:02 -0800

    Hash table remove, search & print

--- algo-practice/hash-table.es6.js
+++ algo-practice/hash-table.es6.js
@@ -16,7 +16,38 @@ class HashTable {
 		this.values[hash][key] = value;
 	}
 
+	remove(key) {
+		const hash = this.calculateHash(key);
+		if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+			delete this.values[hash][key];
+			this.numberOfValues--;
+		}
+	}
+
 	calculateHash(key) {
 		return key.toString().length % this.size;
+	}
+
+	search (key) {
+		const hash = this.calculateHash(key);
+		if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+			return this.values[hash][key];
+		} else {
+			return null;
+		}
+	}
+
+	length() {
+		return this.numberOfValues;
+	}
+
+	print() {
+		let string = '';
+		for (const value in this.values) {
+			for (const key in this.values[value][key]) {
+				string += `${this.values[value][key]}`;
+			}
+		}
+		console.log(string.trim());
 	}
 }
