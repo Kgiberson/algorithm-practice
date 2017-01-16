434359e8982dda26aa17c934223e5a06909c2fcd
Kari Giberson <kari.giberson@gmail.com>
2017-01-16 14:02:33 -0800

    quickSort Part 2 in JS

--- hackerrank/quickSort.js
+++ hackerrank/quickSort.js
@@ -1,4 +1,4 @@
-// Quicksort  Part 1
+// Quicksort  Part 1 - Partition
 'use strict'
 
 let quickSort = arr => {
@@ -20,6 +20,52 @@ let quickSort = arr => {
 }
 
 let printArray = arr => console.log(arr.join(" "));
+
+function processData(input) {
+	let lines = input.split('\n');
+	let arr = lines[1].split(' ').map(i => parseInt(i));
+	quickSort(arr);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+var _input = "";
+process.stdin.on("data", function(input) {
+	_input += input;
+});
+
+process.stdin.on("end", function () {
+	processData(_input);
+})
+
+
+// Quicksort Part 2 - Sorting
+'use strict';
+
+let quickSort = arr => {
+	if (arr.length < 2) {
+		return arr;
+	}
+	let p = arr[0]
+	let left = [];
+	let right = [];
+	let equal = [p];
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] < p) {
+			left.push(arr[i]);
+		} else {
+			right.push(arr[i]);
+		}
+	}
+	left = quickSort(left);
+	right = quickSort(right);
+	let finalArray = left.concat(equal).concat(right);
+	
+	printArray(finalArray);
+	return finalArray;
+}
+
+let printArray = arr => console.log(arr.join(' '));
 
 function processData(input) {
 	let lines = input.split('\n');
